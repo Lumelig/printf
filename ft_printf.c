3f8b4b50f67254771248e57fcaa05a751ccaa9eb
Jenne <pfleghaar.j@gmail.com>
2024-11-09 14:13:58 +0100

    main function

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,10 @@
 /*   By: jenne <jenne@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 16:29:52 by jpflegha          #+#    #+#             */
-/*   Updated: 2024/11/08 22:15:34 by jenne            ###   ########.fr       */
+/*   Updated: 2024/11/09 14:08:31 by jenne            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 #include <stdarg.h>
 #include <unistd.h>
 
@@ -26,10 +25,34 @@ int	print_str(char *str)
 	i = 0;
 	while (*str)
 	{
-		i += write(1, &str, 1);
+		write(1, str, 1);
+		i++;
 		str++;
 	}
 	return (i);
+}
+
+int	print_digit(long n, int base)
+{
+	int		i;
+	char	*symbol;
+
+	symbol = "0123456789sbcdef";
+	i = 0;
+	if (n < 0)
+	{
+		write(1, "-", 1);
+		return (print_digit(-n, base) + 1);
+	}
+	else if (n < base)
+	{
+		return (print_char(symbol[n]));
+	}
+	else
+	{
+		i = print_digit(n / base, base);
+		return (i + print_digit(n % base, base));
+	}
 }
 
 int	print_format(char c, va_list ap)
@@ -42,9 +65,9 @@ int	print_format(char c, va_list ap)
 	else if (c == 's')
 		i = print_str(va_arg(ap, char *));
 	else if (c == 'd')
-		i = print_digit(va_arg(ap, int), 10);
+		i = print_digit((long)(va_arg(ap, int)), 10);
 	else if (c == 'x' || 'X')
-		i = print_digit(va_arg(ap, int), 16);
+		i = print_digit((long)(va_arg(ap, unsigned int)), 16);
 	else
 		i += write(1, &c, 1);
 	return (i);
@@ -57,10 +80,10 @@ int	ft_printf(const char *str, ...)
 
 	i = 0;
 	va_start(ap, str);
-	while (str[i] != '/0')
+	while (*str)
 	{
 		if (*str == '%')
-			i += print_format(*(++str) ap);
+			i += print_format(*(++str), ap);
 		else
 			i += write(1, str, 1);
 		++str;
@@ -71,6 +94,7 @@ int	ft_printf(const char *str, ...)
 
 int	main()
 {
-	ft_printf("halllo");
-	return;
+	char *s = "hallo";
+	ft_printf("halllo %%");
+	return (1);
 }
