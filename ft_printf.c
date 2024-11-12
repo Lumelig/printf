703d3b8dce5542e8911aae9bf92b6fb2b06dd5c1
jpflegha <jpflegha@student.42heilbronn.de>
2024-11-12 21:41:38 +0100

    finsh with main part

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: jpflegha <jpflegha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 16:29:52 by jpflegha          #+#    #+#             */
-/*   Updated: 2024/11/11 17:27:01 by jpflegha         ###   ########.fr       */
+/*   Updated: 2024/11/12 19:23:18 by jpflegha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,15 @@
 #include <unistd.h>
 #include <stdio.h>
 
-int	print_char(int c)
+int	print_char(char c)
 {
-	return (write(1, &c, 1));
+	int	i;
+
+	i = 0;
+	i += write(1, &c, 1);
+	if (i == -1)
+		return (-1);
+	return (i);
 }
 
 int	print_str(char *str)
@@ -25,12 +31,13 @@ int	print_str(char *str)
 
 	i = 0;
 	if (!str)
+	{
 		i = write(1, "(null)", 6);
 		return (i);
+	}
 	while (*str)
 	{
-		write(1, str, 1);
-		i++;
+		i += print_char(*str);
 		str++;
 	}
 	return (i);
@@ -43,20 +50,38 @@ int	print_digit(long n, char c, int base)
 
 	symbol = "0123456789abcdef";
 	i = 0;
-	if (n < 0 && c != 'u')
+	if (n < 0 && c != 'u' && c != 'x' && c != 'X')
 	{
 		i += write(1, "-", 1);
 		n *= -1;
 	}
-	else if (n < base)
+	if (n < base)
 	{
 		if (c == 'X')
 			symbol = "0123456789ABCDEF";
-		return (print_char(symbol[n]));
+		i += print_char(symbol[n]);
+		return (i);
 	}
 	if (n >= base)
 		i += print_digit(n / base, c, base);
 	i += print_digit(n % base, c, base);
+	return (i);
+}
+
+int	print_pointer(unsigned long n, unsigned int base)
+{
+	int				i;
+	char			*symbol;
+
+	symbol = "0123456789abcdef";
+	i = 0;
+	if (n < base)
+	{
+		return (print_char(symbol[n]));
+	}
+	if (n >= base)
+		i += print_pointer(n / base, base);
+	i += print_pointer(n % base, base);
 	return (i);
 }
 
@@ -76,14 +101,14 @@ int	print_format(char c, va_list ap)
 	else if (c == 'p')
 	{
 		i += write(1, "0x", 2);
-		i = print_digit((unsigned long)(va_arg(ap, void *)), 'x', 16);
+		i += print_pointer((unsigned long)va_arg(ap, void *), 16);
 	}
 	else if (c == 'x')
-		i = print_digit((long)(va_arg(ap, unsigned int)), 'x', 16);
+		i += print_digit((long)(va_arg(ap, unsigned int)), 'x', 16);
 	else if (c == 'X')
-		i = print_digit((long)(va_arg(ap, unsigned int)), 'X', 16);
+		i += print_digit((long)(va_arg(ap, unsigned int)), 'X', 16);
 	else
-		i += write(1, &c, 1);
+		i += print_char(c);
 	return (i);
 }
 
@@ -97,7 +122,9 @@ int	ft_printf(const char *str, ...)
 	while (*str)
 	{
 		if (*str == '%')
+		{
 			i += print_format(*(++str), ap);
+		}
 		else
 			i += write(1, str, 1);
 		++str;
@@ -108,11 +135,12 @@ int	ft_printf(const char *str, ...)
 
 int	main()
 {
-    unsigned int i = -42;
+	int j = -15; 
+    unsigned int i = 42;
 	char *s = "hallo";
-    i = ft_printf("%s \n", "some string with %s hehe");
-	printf("%i", i);
-    i = printf("%s \n", "%some string with s hehe");
-	printf("%i", i);
+    i = ft_printf("%");
+	printf("%i \n", i);
+    i = printf("%%%");
+	printf("%i \n", i);
 	return (1);
 }
